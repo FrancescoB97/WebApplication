4187acb64e26faca147fe6ff9098c6de3a2df750
FrancescoB97 <bonini.francesco97@gmail.com>
2021-03-15 21:07:14 +0100

    ex0 completed

--- labs/lab1/ex0/ex0.js
+++ labs/lab1/ex0/ex0.js
@@ -1,20 +1,17 @@
 'use strict'
 
-let array = ["Ciao,", "Come", "Va", "La", "Vita?"] 
-/*let fruits = ["Banana", "Orange", "Lemon", "Apple", "Mango"];
-let citrus = fruits.slice(1, 3); */
+let array = ["Ciao", "Io", "adoro", "le", "stringhe"] 
 
-let array2 = [];
+console.log(array);
 
-
+let i = 0;
 for (const e of array)
-{
-    const index;
-    if(e.length == 4)
-        array2.push("boch")
-
-    console.log(e[0]);
-    console.log(e.slice[0,2]);
+{   
+    const str = e.slice(0,2) + e.slice(e.length-2,e.length); 
+    (e.length >= 4) ? array[i] = str : array[i] = "";          
+    i++;    
 }
+
+console.log(array);
     
 
